e40a33fd3f852a147d1085a7f5c9a0894272b961
minde8888 <58484005+minde8888@users.noreply.github.com>
2020-03-30 23:17:40 +0300

    button

--- js/funktion.js
+++ js/funktion.js
@@ -1,20 +1,5 @@
 "use strict";
 
-
-/*let procent = document.querySelector('.loader');*/
-let button = document.getElementById("clickme")
-
-let count = 0;
-
-button.onclick = function() {
-    count += 1;
-    button.innerHTML = " Click on: " + count;
-};
-/*procent.addEventListener('click', funktion() {
-    count += 1;
-    procent.innerHTML =  count + "% ";
-});*/
-
 let procent = document.querySelector('.loader');
 let button = document.getElementById("clickme")
 
